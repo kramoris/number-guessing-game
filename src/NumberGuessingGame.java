d9c4529b4eed5d99cb5af1c1cdae4082386cab1d
kramoris <139485903+kramoris@users.noreply.github.com>
2025-04-24 15:00:31 +0200

    Add guessing loop and feedback for user

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -7,13 +7,26 @@ public class NumberGuessingGame {
         Random random = new Random();
 
         int secretNumber = random.nextInt(100) + 1;
+        int guess;
+        int attempts = 0;
 
         System.out.println("Welcome to the Number Guessing Game!");
         System.out.println("I'm thinking of a number between 1 and 100.");
 
-        System.out.print("Enter your guess: ");
-        int guess = scanner.nextInt();
+        do {
+            System.out.print("Enter your guess: ");
+            guess = scanner.nextInt();
+            attempts++;
 
-        System.out.println("You guessed: " + guess);
+            if (guess < secretNumber) {
+                System.out.println("Too low!");
+            } else if (guess > secretNumber) {
+                System.out.println("Too high!");
+            } else {
+                System.out.println("Correct! You guessed it in " + attempts + " tries.");
+            }
+        } while (guess != secretNumber);
+
+        scanner.close();
     }
 }
