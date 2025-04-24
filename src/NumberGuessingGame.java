e1ad754053ce3010bd93a7b3a46907601afa9ee3
kramoris <139485903+kramoris@users.noreply.github.com>
2025-04-24 14:48:08 +0200

    Add random number and input prompt

--- src/NumberGuessingGame.java
+++ src/NumberGuessingGame.java
@@ -1,6 +1,19 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class NumberGuessingGame {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int secretNumber = random.nextInt(100) + 1;
+
         System.out.println("Welcome to the Number Guessing Game!");
-        // Game logic will go here later
+        System.out.println("I'm thinking of a number between 1 and 100.");
+
+        System.out.print("Enter your guess: ");
+        int guess = scanner.nextInt();
+
+        System.out.println("You guessed: " + guess);
     }
 }
