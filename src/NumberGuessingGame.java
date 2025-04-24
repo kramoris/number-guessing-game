113ce7c7888d39845096f8b96093fbd90179484a
kramoris <139485903+kramoris@users.noreply.github.com>
2025-04-24 14:46:24 +0200

    Create Java class with main method

---
+++ src/NumberGuessingGame.java
@@ -0,0 +1,6 @@
+public class NumberGuessingGame {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Number Guessing Game!");
+        // Game logic will go here later
+    }
+}
